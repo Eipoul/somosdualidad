8dab8b9cfc055cf42493f1dad3465c0909b8f280
Eipoul <eipoul@hotmail.com>
2026-02-22 18:19:17 +0100

    Fix Payload admin page prop typing for Next build

--- app/admin/[[...segments]]/page.tsx
+++ app/admin/[[...segments]]/page.tsx
@@ -2,20 +2,22 @@ import config from '@/payload.config'
 import { RootPage, generatePageMetadata } from '@payloadcms/next/views'
 
 type AdminPageProps = {
-  params?: Promise<{ segments?: string[] }>
-  searchParams?: Promise<Record<string, string | string[] | undefined>>
+  params: Promise<{ segments?: string[] }>
+  searchParams: Promise<Record<string, string | string[] | undefined>>
 }
 
 export async function generateMetadata(props: AdminPageProps) {
-  const params = (await props.params) ?? {}
-  const searchParams = (await props.searchParams) ?? {}
-
-  return generatePageMetadata({ config, params, searchParams })
+  return generatePageMetadata({
+    config,
+    params: props.params,
+    searchParams: props.searchParams,
+  })
 }
 
 export default async function Page(props: AdminPageProps) {
-  const params = (await props.params) ?? {}
-  const searchParams = (await props.searchParams) ?? {}
-
-  return RootPage({ config, params, searchParams })
+  return RootPage({
+    config,
+    params: props.params,
+    searchParams: props.searchParams,
+  })
 }
