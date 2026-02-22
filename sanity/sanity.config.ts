0b2a4b69c82915fbc85fd625d301f10a668c9aca
Eipoul <eipoul@hotmail.com>
2026-02-22 16:29:52 +0100

    Update sanity.config.ts

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,12 +1,14 @@
-import {visionTool} from '@sanity/vision'
-import {defineConfig} from 'sanity'
-import {presentationTool} from 'sanity/presentation'
-import {structureTool} from 'sanity/structure'
-import {schemaTypes} from './schemaTypes'
-import {structure} from './structure'
+import { visionTool } from '@sanity/vision'
+import { defineConfig } from 'sanity'
+import { presentationTool } from 'sanity/presentation'
+import { structureTool } from 'sanity/structure'
+import { schemaTypes } from './schemaTypes'
+import { structure } from './structure'
 
-const previewUrl = process.env.SANITY_STUDIO_PREVIEW_URL || 'https://somosdualidad.com'
-const previewSecret = process.env.SANITY_PREVIEW_SECRET || process.env.SANITY_STUDIO_PREVIEW_SECRET
+const previewUrl =
+  process.env.SANITY_STUDIO_PREVIEW_URL || 'https://somosdualidad.com'
+
+const previewSecret = process.env.SANITY_STUDIO_PREVIEW_SECRET
 
 export default defineConfig({
   name: 'default',
@@ -14,17 +16,21 @@ export default defineConfig({
   projectId: process.env.SANITY_STUDIO_PROJECT_ID || 'qi7d9rsb',
   dataset: process.env.SANITY_STUDIO_DATASET || 'production',
   plugins: [
-    structureTool({structure}),
+    structureTool({ structure }),
     presentationTool({
       previewUrl: {
-        initial: previewBaseUrl,
+        initial: previewUrl,
         previewMode: {
-          enable: `/api/draft/enable${previewSecret ? `?secret=${encodeURIComponent(previewSecret)}&redirect=/` : ''}`,
+          enable: previewSecret
+            ? `/api/draft/enable?secret=${encodeURIComponent(
+                previewSecret
+              )}&redirect=/`
+            : '/api/draft/enable',
           disable: '/api/draft/disable?redirect=/',
         },
       },
     }),
     visionTool(),
   ],
-  schema: {types: schemaTypes},
+  schema: { types: schemaTypes },
 })
